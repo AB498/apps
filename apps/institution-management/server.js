d5a06eeb22d0e96ecbb1e89487129ca49df88706
AB498 <abcd49800@gmail.com>
2024-12-17 17:42:49 +0600

    m

--- apps/institution-management/server.js
+++ apps/institution-management/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use('/', express.static(path.join(__dirname, './')));
-
-app.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
 
 app.get('/specific', (req, res) => {
     res.send('Specific');
 });
+app.use('/', express.static(path.join(__dirname, './')));
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 
 module.exports = app;
